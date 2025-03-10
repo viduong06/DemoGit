b313035daadf0d054dfcb1dabfeb43850747e90a
viduong06 <viduong214799@gmail.com>
2025-03-10 21:26:37 +0700

    Initial commit

---
+++ .vscode/demo.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<vector>
+
+using namespace std;
+int main ()
+{
+	int n,m;
+	cin>>n>>m;
+	int a[n],b[m];
+	int cnt[100001]={0};
+	for ( int i = 0; i < n; ++i)
+	{
+		cin>>a[i];
+		cnt[a[i]]=-1;
+	}
+	for ( int i = 0; i < m; i++)
+	{
+		cin>>b[i];
+		if(cnt[b[i]]==-1)
+		{
+			cnt[b[i]]=2;
+		}
+		else 
+		{
+			cnt[b[i]]=1;
+		}
+	}
+    for ( int i = 0; i < 100000; i++)
+    {
+    	if ( cnt[i]!=0)
+           cout<<i<<" ";
+	}
+
+}
---
+++ .vscode/map.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<map>
+using namespace std;
+using ll= long long;
+int main()
+{
+    //map.erase():logN
+    // map.find():logN
+    // map.insert:logN
+    // map.begin, map.end()
+    //count():logN
+    // multimap
+    // unordered_map:O(1)=>O(n)
+    // sort(khai bao algorithm): NlogN ( <=10^6) syntax : 1: doi voi mang 1 chieu xet tu x toi y : sort(a+x, a+y+1) 
+    //  2 :doi voi vector ( hay cac cau truc du lieu lam viec voi iterator): [x,y] : sort(a+x, a+y+1)
+    // ham sort dao nguoc : them greater < KIEU_DU_LIEU > ()); 
+    map < ll, int > countMap;
+    int n;
+    cin>>n;
+    vector < ll > a(n);
+    for( int i = 0; i < n; i++)
+    {
+        cin>>a[i];
+        countMap[a[i]]++;
+    }
+    for( int i =0; i < n; i++)
+    {
+        if(countMap[a[i]]!=0)
+        {
+            cout<<a[i]<<" "<<countMap[a[i]]<<endl;
+         
+        }
+        countMap[a[i]]=0;
+    }
+    multimap<ll,int > deleteMap;
+    deleteMap.insert({3,5});
+    deleteMap.insert({3,5});
+    deleteMap.insert({3,5});
+    auto itz= deleteMap.find(3);
+    deleteMap.erase(itz);
+
+    for ( auto it : deleteMap)
+    {
+        cout<<it.first<<" "<<it.second<<endl;
+    }
+    
+   return 0;
+
+}
